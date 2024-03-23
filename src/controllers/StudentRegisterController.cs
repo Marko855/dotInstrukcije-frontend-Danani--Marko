5c695b6b70c986798d5f3c1c339654cbb80fee42
Marko Dananic <markodananic@MacBook-Pro-od-Marko.local>
2024-03-23 22:45:11 +0100

    edited controllers

--- src/controllers/StudentRegisterController.cs
+++ src/controllers/StudentRegisterController.cs
@@ -14,8 +14,6 @@ namespace Instrukcije.Controllers
         [Route("register/student")]
         public IActionResult RegisterStudent([FromBody] StudentRegistrationRequest request)
         {
-            // Ovdje bi trebali dodati logiku za registraciju studenta
-            // Provjera i spremanje podataka, slanje poruka itd.
 
             
             bool success = true; 
